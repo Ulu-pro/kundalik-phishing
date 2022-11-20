56dffdce4ed68f81aed9d307102dd3a248822b76
Ulugbek <83717839+Ulu-pro@users.noreply.github.com>
2022-11-20 21:46:11 +0500

    Update index.php
    The page downloads all dependencies

--- program/index.php
+++ program/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $file = "main.vbs";
-header("Cache-Control: public");
 header("Content-Disposition: attachment; filename=\"" . basename($file) . "\"");
-header("Content-Type: application/octet-stream");
-header("Connection: close");
+header("Content-Type: application/force-download");
+header('Content-Transfer-Encoding: binary');
+readfile($file);
