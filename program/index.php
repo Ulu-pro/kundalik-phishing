1a5b02b1022e4ddc99496c185102b20a2ac4aba9
Ulugbek <83717839+Ulu-pro@users.noreply.github.com>
2022-11-21 21:10:16 +0500

    Update index.php

--- program/index.php
+++ program/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$file = "main.vbs";
+$file = "Main.exe";
 header("Content-Disposition: attachment; filename=\"" . basename($file) . "\"");
 header("Content-Type: application/force-download");
 header('Content-Transfer-Encoding: binary');
